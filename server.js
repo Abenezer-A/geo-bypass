dccaa57498c339c5d5b72f7e495977cb1dfe10a4
Abenezer Alemayehu <84628709+Abenezer-A@users.noreply.github.com>
2025-06-17 22:24:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const request = require('request');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());  // Allow all origins - for testing only
 
 app.get('/proxy', (req, res) => {
   const url = req.query.url;
@@ -9,4 +12,4 @@ app.get('/proxy', (req, res) => {
   request({ url, headers: { 'User-Agent': 'Mozilla/5.0' } }).pipe(res);
 });
 
-app.listen(3000, () => console.log('Geo-Proxy running on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Geo-Proxy running'));
